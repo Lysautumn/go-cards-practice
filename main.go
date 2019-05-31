3f166a4235f13d09a8ec3fc31fdd7d7099dfa5c8
Ally <ally@primeacademy.io>
2019-05-31 13:57:34 -0500

    initial commit of cards tutorial

---
+++ main.go
@@ -0,0 +1,12 @@
+// executable package
+package main
+
+import "fmt"
+
+// will be called when we execute this file
+func main() {
+	// create a variable and give it an assignment
+	var card string = "Ace of Spaces"
+	fmt.Println(card)
+
+}
