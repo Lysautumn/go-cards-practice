431bb7aa6988733d5135bf46de2568b8b9795fe8
Ally <ally@primeacademy.io>
2019-05-31 15:40:24 -0500

    learning about variables

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 // executable package
 package main
 
-import "fmt"
-
 // will be called when we execute this file
 func main() {
 	// create a variable and give it an assignment
-	var card string = "Ace of Spaces"
-	fmt.Println(card)
+	// this is a long and specific way to assign this variable
+	// var card string = "Ace of Spaces"
 
+	// Go compiler  will infer it's a string because of the assignment,
+	// so we can also assign the variable like this:
+	// card := "Ace of Spaces"
+	// The := is only when we are declaring a new variable
+	// When reassigning, we can do `card = "Three of Hearts"`
+
+	// When main is executed, will get return from newCard function
+	// and assign it to card variable
+	// card := newCard()
+	// fmt.Println(card)
+}
+
+// will return value of card variable
+// 'string' after parentheses tells Go compiler to expect function
+// to return a string
+func newCard() string {
+	// returns a string
+	return "Five of Hearts"
 }
