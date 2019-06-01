a09ff7c28f772a5a50d914d8ff160ae9e26e9662
Ally <ally@primeacademy.io>
2019-06-01 04:46:15 -0500

    example of converting string into byte slice

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 // executable package
 package main
 
+import "fmt"
+
 // will be called when we execute this file
 func main() {
-
+	// create a variable to turn into a byte slice
+	greeting := "Hi there!"
+	// use type conversion to print greeting to console
+	// as byte slice
+	fmt.Println([]byte(greeting))
 }
